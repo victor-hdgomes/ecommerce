f2ebaba67c5f5db3dfbe39d7e5b6742b1ba7d2b1
Victor Hugo <88356985+victor-hdgomes@users.noreply.github.com>
2022-10-16 11:49:22 -0300

    Slide part finished

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,12 +1,14 @@
 // Components
 import Announcement from '../../components/Announcement';
 import Navbar from '../../components/Navbar'
+import Slider from '../../components/Slider'
 
 const Home = () => {
     return (
         <div>
             <Announcement />
             <Navbar />
+            <Slider />
         </div>
     )
 }
