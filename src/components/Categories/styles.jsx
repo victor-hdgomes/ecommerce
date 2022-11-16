c1054b09172905c3b5c14cae856c09740db35390
Victor Hugo <88356985+victor-hdgomes@users.noreply.github.com>
2022-11-16 19:13:59 -0300

    Delete src directory

--- src/components/Categories/styles.jsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-import { mobile } from '../../responsive'
-
-export const Container = styled.div`
-    display: flex;
-    padding: 20px;
-    justify-content: space-between;
-    ${mobile({ padding: "0", flexDirection: "column" })};
-`
