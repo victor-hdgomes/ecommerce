b367bc807ec313a59e31a125a0d11c12e7bbddfa
Victor Hugo <88356985+victor-hdgomes@users.noreply.github.com>
2022-11-16 19:53:55 -0300

    Understanding Node.js Router

--- backend/index.js
+++ backend/index.js
@@ -2,13 +2,18 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const UserRoutes = require("./routes/UserRoutes")
 
 dotenv.config();
 
 mongoose
     .connect(process.env.MONGO_URL)
     .then(() => console.log("Mongoose is running"))
-    .catch((err)=>console.log(err))
+    .catch((err) => console.log(err))
+
+app.use(express.json());
+
+app.use("/api/users", UserRoutes);
 
 app.listen(process.env.PORT || 5000, () => {
     console.log("backend is running")
